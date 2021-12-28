0c2e2ba210aa61baf5dde7be6ad399e8c9dcef49
datta kakde <85458867+datta184@users.noreply.github.com>
2021-12-29 02:19:45 +0530

    empty , full array

---
+++ numpy_practice/creating_empty_and_full_numpy_array.py
@@ -0,0 +1,11 @@
+# creating empty and full array 
+import numpy as np
+
+empt = np.empty((3,3), dtype = int)
+
+full = np.full([3,3],55,dtype = int)
+
+print("\nempty array is:")
+print(empt)
+print("\nfull array is:")
+print(full)
